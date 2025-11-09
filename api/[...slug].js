2d4aa05a7be3d84df7c9d54d92179ffd3343e23f
Jakub Nosek <jakubnosek72@gmail.com>
2025-11-09 08:13:33 +0000

    chore: move backend files from api/backend into api/ root; update wrapper & scripts

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,9 +1,8 @@
 import serverless from 'serverless-http'
-import app from './backend/server.js'
+import app from './server.js'
 
 const handler = serverless(app)
 
 export default async function (req, res) {
-  // serverless-http returns a handler compatible with Node.js req/res
   return handler(req, res)
 }
